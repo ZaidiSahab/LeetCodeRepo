c9ec4772cd9ade9dd842acd91c6f62fefdc03900
ZaidiSahab <1mustafazaidi@gmail.com>
2024-08-29 16:47:27 +0500

    Parenthesis Query solved For LeetCode !!

--- leetcode.js
+++ leetcode.js
@@ -11,6 +11,9 @@
 // If it's an opening bracket, push it onto the stack
 // If the stack is empty, all brackets were matched correctly
 
+
+
+var isValid = function(s) {
 let stack = []
 
 // Map to store matching pairs of brackets
@@ -18,17 +21,31 @@ let map = {
     ")": "(",
     "]": "[",
     "}": "{"
-}
+};
 
 
 // Iterate through each character in the string using a traditional for loop
-for (let i = 0; i < s.length(); i++) {
+for (let i = 0; i < s.length; i++) {
     const char = s[i]
 //If the character is a closing bracket
     if (char in map) {
+        // Pop the top element from the stack (if any)
+     const topElem = stack.length> 0 ? stack.pop() :null
+// Check if the popped element matches the corresponding opening bracket
 
-    }
+if(topElem !== map[char])
+{
+    return false
 }
+      
+    }
+    else {
+        stack.push(char)
+     }
+
+}
+return stack.length === 0;
+};
 
 
 
